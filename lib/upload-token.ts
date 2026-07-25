67fe29066cbf643756127730e2a57c26bfbbe482
Rémi <58393254+DoctorPok42@users.noreply.github.com>
2026-07-25 18:29:30 +0200

    Update upload-token.ts

--- lib/upload-token.ts
+++ lib/upload-token.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const SECRET = process.env.JWT_SECRET;
 if (!SECRET) {
-  throw new Error("JWT_SECRET is not set in environment variables");
+  console.error("JWT_SECRET is not defined in environment variables.");
 }
 
 export interface UploadTokenPayload {
